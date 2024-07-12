f765a14686be3584a3b087d613d9a3edeb7eb330
AlpOzgun <154626614+AlpOzgun@users.noreply.github.com>
2024-07-12 13:54:06 +0300

    initial
    initial

---
+++ iks_satınalım_service/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace iks_satınalım_service.Entities
+{
+	public class User
+	{
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+		public int UserId { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(1000)")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(1000)")]
+        public string Lastname { get; set; }
+
+	}
+
+}
+
